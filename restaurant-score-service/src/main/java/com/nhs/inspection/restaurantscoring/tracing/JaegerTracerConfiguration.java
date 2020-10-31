15ed4153f944fb27f0e6d7cd5ecd960a51ed4b21
Lokesh <lokeshraghuram@gmail.com>
2020-10-31 13:40:52 +0530

    Jaeger fix

--- restaurant-score-service/src/main/java/com/nhs/inspection/restaurantscoring/tracing/JaegerTracerConfiguration.java
+++ restaurant-score-service/src/main/java/com/nhs/inspection/restaurantscoring/tracing/JaegerTracerConfiguration.java
@@ -6,10 +6,12 @@ import io.jaegertracing.internal.samplers.ConstSampler;
 import io.jaegertracing.spi.Reporter;
 import io.jaegertracing.spi.Sampler;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(value = "opentracing.jaeger.enabled", havingValue = "false", matchIfMissing = false)
 public class JaegerTracerConfiguration {
     @Value("${spring.application.name}")
     private String applicationName;
