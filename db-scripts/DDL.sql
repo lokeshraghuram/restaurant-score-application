3ee9fa5e22521ecf3be87f0f04b27b3628751b1e
Lokesh <lokeshraghuram@gmail.com>
2020-11-01 12:36:34 +0530

    enhancements

--- db-scripts/DDL.sql
+++ db-scripts/DDL.sql
@@ -1,3 +1,10 @@
+create table inspection.restaurant_status (
+	business_id varchar(255) not null,
+	inspection_date timestamp with time zone,
+	status varchar(255),
+	PRIMARY KEY (business_id)
+);
+
 create table inspection.restaurant_data (
         business_id varchar(255) not null,
         business_name varchar(255),
@@ -13,15 +20,13 @@ create table inspection.restaurant_data (
 		inspection_date timestamp with time zone,
 		inspection_score decimal,
 		inspection_type varchar(255),
-		violation_id varchar(255), 
+		violation_id varchar(255),
 		violation_description varchar(255),
-		risk_category varchar(255),        
-        CONSTRAINT PK_Person PRIMARY KEY (business_id,inspection_id,violation_id)
-    );
-	
-create table inspection.restaurant_status (
-	business_id varchar(255) not null,
-	inspection_date timestamp with time zone,
-	status varchar(255),
-	PRIMARY KEY (business_id)
+		risk_category varchar(255),
+        CONSTRAINT PK_business PRIMARY KEY (business_id,inspection_id,violation_id)
+        /* Uncomment below two lines if data setup is done in restaurant_status table. For casual testing of the application, I commented foreign key relationship */
+        /*,*/
+		/*CONSTRAINT fk_business FOREIGN KEY(business_id) REFERENCES inspection.restaurant_status(business_id)*/
 );
+
+	
