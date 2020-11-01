3f8ee81259b6e0ccd2d692b936b08aa269050f95
Lokesh <lokeshraghuram@gmail.com>
2020-11-01 12:55:00 +0530

    actuator

--- restaurant-score-service/src/main/java/com/nhs/inspection/restaurantscoring/security/SecurityConfig.java
+++ restaurant-score-service/src/main/java/com/nhs/inspection/restaurantscoring/security/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/v3/api-docs",
+                        "/actuator/**",
                         "index",
                         "/css/*",
                         "/js/*").permitAll()
